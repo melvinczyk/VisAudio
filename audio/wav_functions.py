345b2963c62dbdc430087e73a2b99a5493ac223a
Nick Burczyk <78186753+melvinczyk@users.noreply.github.com>
2024-08-05 13:27:56 -0500

    Spectrogram visual

---
+++ audio/wav_functions.py
@@ -0,0 +1,24 @@
+import os.path
+
+import librosa
+import librosa.feature
+from matplotlib import  pyplot as plt
+import numpy as np
+from PIL import Image, ImageTk
+
+
+def generate_visual_spectrogram(file_path):
+    plt.figure(figsize=(6, 2.51))
+    signal, sr = librosa.load(file_path)
+    mel_spec = librosa.power_to_db(np.abs(librosa.stft(y=signal, n_fft=2048, hop_length=512)))
+    librosa.display.specshow(mel_spec, fmax=8000,cmap='magma')
+    # plt.colorbar(format='%+2.0f dB')
+    # plt.title(f"{os.path.basename(file_path)}")
+    plt.savefig('../temp/spectrogram.png', bbox_inches='tight', pad_inches=0)
+
+
+def load_spectrogram():
+    with Image.open('../temp/spectrogram.png') as img:
+        photo = ImageTk.PhotoImage(img)
+        return photo
+
